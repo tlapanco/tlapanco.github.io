656e971ad7d31efc1d4b8dff071459997a79f2ca
tl <99222342+tlapanco@users.noreply.github.com>
2026-04-21 22:40:10 -0500

    first commit

---
+++ src/constants/experience.ts
@@ -0,0 +1,30 @@
+import type { Experience } from "../types/experience";
+
+export const EXPERIENCES : Array<Experience> = [
+	{
+		company: 'UPMP',
+		position: 'Desarrollador web',
+		period: 'Enero 2022 - Diciembre 2022',
+		description: 'Parte del proyecto integrador de la UPMP módulo vinculación, donde se llevó a cabo el desarrollo del sitio web de la universidad.',
+	},
+	{
+		company: 'ICO',
+		position: 'Docente',
+		period: 'Abril 2023 - Noviembre 2024',
+		description: 'Docente de la carrera de Desarrollador de sistemas y Redes',
+	},
+	{
+		company: 'Freelance',
+		position: 'Freelance',
+		period: 'Noviembre 2024 - Marzo 2025',
+		description: 'Desarrollador independiente de soluciones web, desde el BACK-END hasta el FRONT-END',
+	},
+	{
+		company: 'Tech Marti',
+		position: 'Desarrollador Full Stack',
+		period: 'Abril 2025 - Marzo 2026',
+		description: 'Desarrollador Full Stack con experiencia en interfaces de escritorio nativas, creación de bases de datos, desarrollo backend y construcción de interfaces web modernas.',
+	}
+]
+
+export const URL_PDF = 'https://tl-developer-site.infinityfreeapp.com/tl/cv.pdf';
